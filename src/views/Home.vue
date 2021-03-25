0d19f94dbb21592a61eb345d7c8656d83a0cfe69
a-dagnicourt <dagnicourt.@gmail.com>
2021-03-25 17:26:37 +0100

    added fonts, placeholders bg images and positionned items

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,24 +1,22 @@
 <template>
-  <main class="home">
-    <section class="infos">
-      <div class="mission-title">
-        <h1>Mission Title</h1>
+  <main class="home flex p-5 h-full bg-opacity-25">
+    <section class="patch w-1/3" />
+    <section class="w-2/3 border-solid border-l-8 border-yellow-400 p-3 flex flex-col">
+      <div class="text-left mt-20 mb-5">
+        <h1 class="font-display text-6xl font-black tracking-wider">
+          Mission Title
+        </h1>
       </div>
-      <div class="mission-desc">
-        <p>
+      <div class="text-left text-xl italic leading-5 my-5">
+        <p class="text-left">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, voluptatem?
           Exercitationem odit assumenda totam, nihil suscipit earum placeat est id quis corporis
           obcaecati at, natus quam, nisi labore tenetur minus!
         </p>
       </div>
-      <div class="date-group">
-        <div class="datetime">25/03/21 21:00 UTC</div>
-        <div class="countdown">1 day 3 hours 30 minutes</div>
-      </div>
-    </section>
-    <section class="patch">
-      <div class="patch-img">
-        <img src="#" alt="patch" />
+      <div class="flex-grow flex flex-col justify-center">
+        <div class="text-left text-4xl">25/03/21 21:00 UTC</div>
+        <div class="text-left text-6xl">1d:3h:30m</div>
       </div>
     </section>
   </main>
@@ -29,3 +27,16 @@ export default {
   name: 'Home',
 };
 </script>
+
+<style>
+main {
+  background-size: cover;
+  background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.95)),
+    url('https://farm4.staticflickr.com/3955/32915197674_eee74d81bb_b.jpg');
+}
+
+.patch {
+  background: left / cover no-repeat url('https://i.imgur.com/573IfGk.png');
+  animation: animatedBackground 10s linear infinite alternate;
+}
+</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@ module.exports = {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: 'media', // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      display: ['Oswald', 'ui-sans-serif', 'system-ui'],
+      body: ['Poppins', 'ui-sans-serif', 'system-ui'],
+    },
     extend: {},
   },
   variants: {
