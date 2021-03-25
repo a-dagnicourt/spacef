f2688e9994fc461542726e330ef7ff3ef2fd0a7b
a-dagnicourt <dagnicourt.@gmail.com>
2021-03-25 14:26:13 +0100

    added html skeleton for SPA + added axios

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,27 @@
 <template>
-  <div class="home"></div>
+  <main class="home">
+    <section class="infos">
+      <div class="mission-title">
+        <h1>Mission Title</h1>
+      </div>
+      <div class="mission-desc">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, voluptatem?
+          Exercitationem odit assumenda totam, nihil suscipit earum placeat est id quis corporis
+          obcaecati at, natus quam, nisi labore tenetur minus!
+        </p>
+      </div>
+      <div class="date-group">
+        <div class="datetime">25/03/21 21:00 UTC</div>
+        <div class="countdown">1 day 3 hours 30 minutes</div>
+      </div>
+    </section>
+    <section class="patch">
+      <div class="patch-img">
+        <img src="#" alt="patch" />
+      </div>
+    </section>
+  </main>
 </template>
 
 <script>
