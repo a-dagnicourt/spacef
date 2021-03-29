09adb61f1bfd64383f25dbdb02ac3e04ded1572a
a-dagnicourt <dagnicourt.@gmail.com>
2021-03-29 17:20:23 +0200

    added launchpad infos

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,4 +1,5 @@
 <script>
+/* eslint-disable arrow-parens */
 import axios from 'axios';
 import dayjs from 'dayjs';
 
@@ -18,14 +19,19 @@ export default {
   methods: {
     async getDatas() {
       const self = this;
-      // eslint-disable-next-line arrow-parens
-      await axios.get('https://api.spacexdata.com/v4/launches/latest').then(res => {
-        self.info = res.data;
-        // console.log(self.info);
+      await axios.get('https://api.spacexdata.com/v4/launches/next').then(res => {
+        self.launch = res.data;
+        // console.log(self.launch);
         self.loading = false;
-        self.formattedDate = dayjs(res.data.date_local).format('D/MM/YY [-] HH:mm');
+        self.formattedDate = dayjs(res.data.date_local).format('DD/MM/YY [-] HH:mm');
         self.countdown = dayjs(res.data.date_local).toNow(true);
       });
+      await axios
+        .get(`https://api.spacexdata.com/v4/launchpads/${self.launch.launchpad}`)
+        .then(res => {
+          self.launchpad = res.data.full_name;
+          console.log(self.launchpad);
+        });
     },
   },
   mounted() {
@@ -41,22 +47,25 @@ export default {
   <main v-else class="home flex p-5 h-full">
     <section class="mission-patch w-1/3 z-0">
       <img
-        :src="info.links.patch.large"
-        :alt="info.name"
+        :src="launch.links.patch.large"
+        :alt="launch.name"
         class="h-full absolute animate-spin-slow"
       />
     </section>
     <section
-      class="mission-infos w-2/3 border-solid border-l-8 border-yellow-400 p-3 flex flex-col z-10"
+      class="mission-launchs w-2/3 border-solid border-l-8 border-yellow-400 p-3 flex flex-col z-10"
     >
       <div class="mission-title text-left mt-20 mb-5">
         <h1 class="font-display text-6xl font-black tracking-wider">
-          {{ info.name }}
+          {{ launch.name }}
         </h1>
+      </div>
+      <div class="launchpad text-left mb-5">
+        {{ launchpad }}
       </div>
       <div class="mission-desc text-left text-xl italic leading-5 my-5">
         <p class="text-left w-2/3">
-          {{ info.details }}
+          {{ launch.details }}
         </p>
       </div>
       <div class="mission-dates flex-grow flex flex-col justify-center">
@@ -71,7 +80,7 @@ export default {
 
 <style>
 main,
-.mission-infos {
+.mission-launchs {
   background-size: cover;
   background-attachment: fixed;
   background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.95)),
