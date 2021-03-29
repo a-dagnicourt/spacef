04223efd3bead5eb6aeb97ecbb9f5e41cab63b5d
a-dagnicourt <dagnicourt.@gmail.com>
2021-03-29 16:41:42 +0200

    added axios to fetch data from spacex api + added time handling package

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
+import axios from 'axios';
+
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import './index.css';
 
-createApp(App)
+createApp(App, axios)
   .use(router)
   .mount('#app');
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,32 +1,66 @@
+<script>
+import axios from 'axios';
+import dayjs from 'dayjs';
+
+const relativeTime = require('dayjs/plugin/relativeTime');
+
+dayjs.extend(relativeTime);
+
+export default {
+  name: 'Home',
+  data() {
+    return {
+      loading: true,
+      formattedDate: null,
+      countdown: null,
+    };
+  },
+  methods: {
+    async getDatas() {
+      const self = this;
+      // eslint-disable-next-line arrow-parens
+      await axios.get('https://api.spacexdata.com/v4/launches/latest').then(res => {
+        self.info = res.data;
+        self.loading = false;
+        self.formattedDate = dayjs(res.data.date_local).format('D/MM/YY [-] HH:mm');
+        self.countdown = dayjs(res.data.date_local).toNow(true);
+      });
+    },
+  },
+  mounted() {
+    this.getDatas();
+  },
+};
+</script>
+
 <template>
-  <main class="home flex p-5 h-full bg-opacity-25">
-    <section class="patch w-1/3" />
-    <section class="w-2/3 border-solid border-l-8 border-yellow-400 p-3 flex flex-col">
-      <div class="text-left mt-20 mb-5">
+  <div class="bg-black h-full flex flex-col items-center justify-center" v-if="loading">
+    <span class="animate-pulse">Loading...</span>
+  </div>
+  <main v-else class="home flex p-5 h-full">
+    <section class="mission-patch w-1/3" />
+    <section
+      class="mission-infos w-2/3 border-solid border-l-8 border-yellow-400 p-3 flex flex-col"
+    >
+      <div class="mission-title text-left mt-20 mb-5">
         <h1 class="font-display text-6xl font-black tracking-wider">
-          Mission Title
+          {{ info.name }}
         </h1>
       </div>
-      <div class="text-left text-xl italic leading-5 my-5">
+      <div class="mission-desc text-left text-xl italic leading-5 my-5">
         <p class="text-left">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, voluptatem?
-          Exercitationem odit assumenda totam, nihil suscipit earum placeat est id quis corporis
-          obcaecati at, natus quam, nisi labore tenetur minus!
+          {{ info.details }}
         </p>
       </div>
-      <div class="flex-grow flex flex-col justify-center">
-        <div class="text-left text-4xl">25/03/21 21:00 UTC</div>
-        <div class="text-left text-6xl">1d:3h:30m</div>
+      <div class="mission-dates flex-grow flex flex-col justify-center">
+        <div class="text-left text-4xl">
+          {{ formattedDate }}
+        </div>
+        <div class="text-left text-6xl">{{ countdown }}</div>
       </div>
     </section>
   </main>
 </template>
-
-<script>
-export default {
-  name: 'Home',
-};
-</script>
 
 <style>
 main {
@@ -35,7 +69,7 @@ main {
     url('https://farm4.staticflickr.com/3955/32915197674_eee74d81bb_b.jpg');
 }
 
-.patch {
+.mission-patch {
   background: left / cover no-repeat url('https://i.imgur.com/573IfGk.png');
   animation: animatedBackground 10s linear infinite alternate;
 }
