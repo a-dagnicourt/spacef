f804dee99f616b610432240b6a8e9d4f30b01c91
a-dagnicourt <dagnicourt.@gmail.com>
2021-05-03 11:30:58 +0200

    reorganized div layers to make text selectionnable + changed marker to circle

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,33 +1,40 @@
 <template>
   <l-map
-    style="height:100vh; width:66.26vw; mix-blend-mode:lighten"
+    style="height: 100vh; width: 66.26vw; right: 0; mix-blend-mode: lighten"
     v-model="zoom"
     v-model:zoom="zoom"
-    :center="[28.5618571, -80.577366 - 20]"
+    :center="[28.5618571, -80.577366 - 40]"
     :options="{ zoomControl: false, scrollWheelZoom: false }"
   >
     <l-tile-layer
       :attribution="attribution"
       url="https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png"
     />
-    <l-marker :lat-lng="[28.5618571, -80.577366]" />
+    <l-circle-marker :lat-lng="circle.center" :radius="circle.radius" :color="circle.color" />
   </l-map>
 </template>
 
 <script>
-import { LMap, LTileLayer, LMarker } from '@vue-leaflet/vue-leaflet';
+import { LMap, LTileLayer, LCircleMarker } from '@vue-leaflet/vue-leaflet';
 import 'leaflet/dist/leaflet.css';
 
 export default {
+  name: 'Map',
   components: {
     LMap,
     LTileLayer,
-    LMarker,
+    LCircleMarker,
   },
   data() {
     return {
       zoom: 4,
       attribution: 'Map tiles by Carto, under CC BY 3.0. Data by OpenStreetMap, under ODbL.',
+      circle: {
+        center: [28.5618571, -80.577366],
+        radius: 10,
+        color: 'rgba(251, 191, 36, 0.65)',
+        interactive: true,
+      },
     };
   },
 };
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,4 @@
 <script>
-/* eslint-disable arrow-parens */
 import axios from 'axios';
 import dayjs from 'dayjs';
 
@@ -25,7 +24,7 @@ export default {
   computed: {
     setBg() {
       return {
-        backgroundImage: `linear-gradient(to right, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 75%), url(${this.rocket})`,
+        backgroundImage: `linear-gradient(to right, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 80%), url(${this.rocket})`,
       };
     },
   },
@@ -34,30 +33,30 @@ export default {
       const self = this;
       await axios
         .get('https://api.spacexdata.com/v4/launches/next')
-        .then(res => {
+        .then((res) => {
           self.launch = res.data;
           console.log(self.launch);
           self.loading = false;
           self.formattedDate = dayjs(res.data.date_local).format('DD/MM/YY [-] HH:mm');
           self.countdown = dayjs(res.data.date_local).toNow(true);
         })
-        .catch(err => {
+        .catch((err) => {
           console.log(err);
         });
       await axios
         .get(`https://api.spacexdata.com/v4/launchpads/${self.launch.launchpad}`)
-        .then(res => {
+        .then((res) => {
           self.launchpad = res.data.full_name;
         })
-        .catch(err => {
+        .catch((err) => {
           console.log(err);
         });
       await axios
         .get(`https://api.spacexdata.com/v4/rockets/${self.launch.rocket}`)
-        .then(res => {
+        .then((res) => {
           self.rocket = res.data.flickr_images[Math.floor(Math.random() * res.data.flickr_images.length)];
         })
-        .catch(err => {
+        .catch((err) => {
           console.log(err);
         });
     },
@@ -69,47 +68,50 @@ export default {
 </script>
 
 <template>
-  <div class="bg-black h-full flex flex-col items-center justify-center" v-if="loading">
-    <span class="animate-pulse">Loading...</span>
+  <div class="wrapper bg" :style="setBg">
+    <div class="content bg h-full w-full absolute z-30" :style="setBg">
+      <div class="bg-black h-full flex flex-col items-center justify-center" v-if="loading">
+        <span class="animate-pulse">Loading...</span>
+      </div>
+      <main v-else class="home flex h-full z-30">
+        <section id="mission-patch" class="w-full z-0 overflow-hidden">
+          <img :src="launch.links.patch.large" :alt="launch.name" class="absolute max-h-full animate-spin-slow" />
+        </section>
+        <Map class="z-20 absolute" />
+        <section
+          id="mission-launchs"
+          class="bg w-2/3 left-1/3 border-solid border-l-8 border-yellow-400 flex flex-col absolute h-full"
+          :style="setBg"
+        >
+          <div id="mission-title" class="text-left mt-20 mb-5 ml-3 z-30">
+            <h1 class="font-display text-6xl font-black tracking-wider text-yellow-400">
+              <i class="fas fa-rocket"></i>{{ launch.name }}
+            </h1>
+          </div>
+          <div class="mission-desc text-left text-xl italic leading-5 my-5 ml-3 z-30">
+            <p class="text-left w-2/3">
+              {{ launch.details }}
+            </p>
+          </div>
+          <div id="mission-dates" class="flex-grow flex flex-col justify-center ml-3 z-30">
+            <div class="text-left text-4xl pl-8">
+              {{ formattedDate }}
+            </div>
+            <h2 class="text-left text-9xl absolute pt-16 opacity-25 font-display tracking-wide font-black z-30">
+              {{ countdown }}
+            </h2>
+          </div>
+        </section>
+        <!-- <div>
+      <p class="relative h-full z-30">Ola fdp comment ca va ?</p>
+    </div> -->
+      </main>
+    </div>
   </div>
-  <main v-else class="home flex h-full relative" :style="setBg">
-    <section id="mission-patch" class="w-full z-0 relative overflow-hidden">
-      <img :src="launch.links.patch.large" :alt="launch.name" class="absolute max-h-full animate-spin-slow" />
-    </section>
-    <section
-      id="mission-launchs"
-      class="w-2/3 left-1/3 border-solid border-l-8 border-yellow-400 flex flex-col z-10 absolute h-full"
-      :style="setBg"
-    >
-      <Map class="absolute z-20" />
-      <div id="mission-title" class="text-left mt-20 mb-5 ml-3">
-        <h1 class="font-display text-6xl font-black tracking-wider text-yellow-400 z-top">
-          <i class="fas fa-rocket"></i>{{ launch.name }}
-        </h1>
-      </div>
-      <div class="launchpad text-left mb-5 ml-3">
-        {{ launchpad }}
-      </div>
-      <div class="mission-desc text-left text-xl italic leading-5 my-5 ml-3">
-        <p class="text-left w-2/3">
-          {{ launch.details }}
-        </p>
-      </div>
-      <div id="mission-dates" class="flex-grow flex flex-col justify-center ml-3">
-        <div class="text-left text-4xl pl-8">
-          {{ formattedDate }}
-        </div>
-        <h2 class="text-left text-9xl absolute pt-16 opacity-25 font-display tracking-wide font-black">
-          {{ countdown }}
-        </h2>
-      </div>
-    </section>
-  </main>
 </template>
 
 <style>
-main,
-#mission-launchs {
+.bg {
   background-size: cover;
   background-attachment: fixed;
 }
